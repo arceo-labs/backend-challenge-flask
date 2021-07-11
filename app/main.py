58f2318da4f8f2819786eb4739aac57e41923b3a
Russell Snyder <russ@arceo.ai>
2021-07-11 19:51:11 -0400

    Updated gitignore; enabled running using flask CLI

--- app/main.py
+++ app/main.py
@@ -10,13 +10,14 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
 
+models.db.init_app(app)
+schemas.ma.init_app(app)
+resources.api.init_app(app)
+
 @app.before_first_request
 def create_tables():
     models.db.create_all()
 
 
 if __name__ == "__main__":
-    models.db.init_app(app)
-    schemas.ma.init_app(app)
-    resources.api.init_app(app)
     app.run(port=5000, debug=True)
